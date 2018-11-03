298d0bb9a9bca670cb4711f2a9df7661404c57dc
nathanscott85 <nathansfeely@gmail.com>
2018-11-03 15:27:19 +0000

    Updates

---
+++ precache-manifest.8babe381bb88e728405281a719220511.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "299c31fc3d96e3eeab37",
+    "url": "/potterquotes/static/js/runtime~main.299c31fc.js"
+  },
+  {
+    "revision": "b1f53447ec1f4bc896b1",
+    "url": "/potterquotes/static/js/main.b1f53447.chunk.js"
+  },
+  {
+    "revision": "d7c3259394c4b01131ba",
+    "url": "/potterquotes/static/js/1.d7c32593.chunk.js"
+  },
+  {
+    "revision": "b1f53447ec1f4bc896b1",
+    "url": "/potterquotes/static/css/main.20dd1bd4.chunk.css"
+  },
+  {
+    "revision": "a11fc70e1f4114b40efecd868c763843",
+    "url": "/potterquotes/index.html"
+  }
+];
